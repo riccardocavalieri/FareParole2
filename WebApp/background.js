3b6fabb337516fb909139603357feb1d98f8a514
Luca Verrascina <lucaverrascina@hotmail.it>
2017-11-15 01:46:15 +0100

    Fix Chrome Apps and generate updated package

--- WebApp/background.js
+++ WebApp/background.js
@@ -1,11 +1,13 @@
 chrome.app.runtime.onLaunched.addListener(function() {
-  //chrome.app.window.create('index.html', {
-  //  // frame: "none",
-  //  // state: "fullscreen",
-  //  'outerBounds': {
-  //    'width': 1200,
-  //    'height': 800
-  //  }
-    //});
-    window.open('index.html');
+	
+	chrome.app.window.create('index.html', {
+		//frame: "none",
+		//state: "fullscreen",
+		'outerBounds': {
+      		'width': 980,
+      		'height': 800
+    	}
+    });
+    
+    //window.open('index.html');
 });
